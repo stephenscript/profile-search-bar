b2a0cfa222038bce9f8df8afeb7d2eff6f9555ea
Stephen <stephen.anthony.rivas@gmail.com>
2023-05-18 16:51:04 -0400

    connected to algoliasearch and created basic mentor search functionality

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -1,16 +1,65 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./SearchBar.css";
 import SearchPopup from "../SearchPopup/SearchPopup";
 import { magnifyingGlass, xButton } from "../assets/svgs";
+import algoliasearch, {
+  Client,
+} from "https://cdn.jsdelivr.net/npm/algoliasearch@4.17.0/dist/algoliasearch-lite.esm.browser.js";
+
+const getProfiles = async (client: Client, setMentors: ([]) => void, input) => {
+  try {
+    const res = await client.search([input]);
+    console.log(res.hits);
+    const hits = res.hits.map((hit: any) => {
+      return (
+        <>
+          <div key={hit.ObjectID}>
+            <h3>{hit.name}</h3>
+            <h4>{hit.credentials}</h4>
+          </div>
+        </>
+      );
+    });
+    setMentors(hits);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function SearchBar() {
+  const algolia = useRef(
+    algoliasearch(
+      import.meta.env.VITE_ALGOLIA_ID,
+      import.meta.env.VITE_ALGOLIA_API_KEY
+    )
+  );
+  const mentorsIndex = useRef(
+    algolia.current.initIndex("production_user_profiles")
+  );
+  const [input, setInput] = useState("");
+  const [mentors, setMentors] = useState([]);
+
+  useEffect(() => {
+    if (!input.length) return;
+    const id = setTimeout(
+      () => getProfiles(mentorsIndex.current, setMentors, input),
+      500
+    );
+    return () => clearTimeout(id);
+  }, [input]);
+
   return (
     <>
       <div className="bar">
-        <input id="search" />
-        {magnifyingGlass}
-        {xButton}
+        <input
+          id="search"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div id="mag">{magnifyingGlass}</div>
+        <div id="xb">{xButton}</div>
       </div>
+      {mentors}
       <SearchPopup />
     </>
   );
--- src/assets/svgs.tsx
+++ src/assets/svgs.tsx
@@ -1,5 +1,5 @@
 export const magnifyingGlass = (
-  <svg id="mag"
+  <svg 
     width="19"
     height="19"
     viewBox="0 0 19 19"
@@ -7,8 +7,8 @@ export const magnifyingGlass = (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M12.0492 13.4633C10.7874 14.4273 9.21056 15 7.5 15C3.35786 15 0 11.6421 0 7.5C0 3.35786 3.35786 0 7.5 0C11.6421 0 15 3.35786 15 7.5C15 9.21051 14.4274 10.7873 13.4634 12.0491L18.364 16.9497C18.7545 17.3402 18.7545 17.9734 18.364 18.3639C17.9735 18.7544 17.3403 18.7544 16.9498 18.3639L12.0492 13.4633ZM13 7.5C13 10.5376 10.5376 13 7.5 13C4.46243 13 2 10.5376 2 7.5C2 4.46243 4.46243 2 7.5 2C10.5376 2 13 4.46243 13 7.5Z"
       fill="#181819"
     />
@@ -16,7 +16,7 @@ export const magnifyingGlass = (
 );
 
 export const xButton = (
-  <svg id="xb"
+  <svg
     width="16"
     height="16"
     viewBox="0 0 16 16"
@@ -45,7 +45,7 @@ export const xButton = (
 );
 
 export const octothorp = (
-  <svg id="octo"
+  <svg 
     width="16"
     height="16"
     viewBox="0 0 16 16"
@@ -60,7 +60,7 @@ export const octothorp = (
 );
 
 export const letterT = (
-  <svg id="letterT"
+  <svg
     width="16"
     height="16"
     viewBox="0 0 16 16"
